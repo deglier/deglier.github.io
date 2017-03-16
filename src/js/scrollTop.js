6f511ade218fd9a2438f83563e6e6700980e8558
Deglier Amorim <degliersa@gmail.com>
2017-03-16 13:39:26 -0300

    removendo post antigos e definir color theme para as categorias novas

---
+++ src/js/scrollTop.js
@@ -0,0 +1,10 @@
+(function($, document){
+
+$(document).ready(function() { 
+    $(".scroll").click(function(event){        
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+   });
+});
+
+})(jQuery, document)
